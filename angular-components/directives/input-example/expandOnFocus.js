bdce6fcd5bfb0105674d8c8855dadb59f425c61f
BJ Clark <bjclark13@outlook.com>
2019-04-11 18:21:41 -0400

    Add directives exercise example

---
+++ angular-components/directives/input-example/expandOnFocus.js
@@ -0,0 +1,31 @@
+function ExpandOnFocus() {
+    return {
+        restrict: "A",
+        scope: {
+            count: '='
+        },
+        link: function($scope, $element, $attrs) {
+            let height = 50;
+
+            $scope.$watch($attrs.count, function(value) {
+                height = value;
+                console.log(value);
+            });
+            
+            $element.on("focus", () => {
+                console.log($scope, $element, $attrs);
+
+                console.log(height);
+                
+                $element.css("height", height + 'px');
+            });
+            $element.on("blur", () => {
+                $element.css("height", "");
+            });
+        }
+    };
+};
+
+angular
+.module('InputExample')
+.directive('expandOnFocus', ExpandOnFocus);
---
+++ angular-components/directives/input-example/upDownComponent.js
@@ -0,0 +1,43 @@
+function component() {
+    function componentController($scope){
+        var vm = this;
+        
+        $scope.count = 0;
+
+        vm.goUp = () => {
+            $scope.count += 10;
+        }
+
+        vm.goDown = () => {
+            $scope.count -= 10;
+        }
+    }
+
+    return {
+        bindings: {},
+        controller: componentController,
+        template: `
+            <div>
+                <button ng-click="$ctrl.goUp()">Up</button>
+            </div>
+            <div>{{count}}</div>
+            <div>
+                <button ng-click="$ctrl.goDown()">Down</button>
+            </div>
+
+            <div>
+            <label>
+                Click inside of the input to expand it
+                <p>
+                <input expand-on-focus count="count" />
+                </p>
+                
+            </label>
+        </div>
+        `
+    }
+}
+
+angular
+.module ('InputExample')
+.component ('upDownCounter', component());

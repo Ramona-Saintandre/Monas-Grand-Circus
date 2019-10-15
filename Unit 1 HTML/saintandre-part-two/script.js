0e40bfade4e9ae88334e3a9a260a8f9cf092d446
Ramona Saintandre <thenewmona@gmail.com>
2019-10-14 20:00:56 -0400

    created demo html file

--- Unit 1 HTML/saintandre-part-two/script.js
+++ Unit 1 HTML/saintandre-part-two/script.js
@@ -1,6 +1,6 @@
 // Declare the variables
 
-let eventType = prompt("what kind of event are you going to ?","casual,semi-formal,formal");
+let eventType = prompt("what kind of event are you going to ?","casual ,semi-formal ,formal ");
 
 let tempFahr = prompt("What is the tempature outside?" , "In Fahrenheit tempature");
 
@@ -26,6 +26,10 @@ if (tempFahr <54){
 
 // prompt out 
 
-let result = `Since it is $(tempFahr) degrees and you are going to a $(eventType) event, you should wear $(clothingType + tempClothType)`;
+
+    let result = `Since it is $(tempFahr) degrees and you are going to a $(eventType) event, you should wear $(clothingType + tempClothType)`;
 
 console.log (result);
+
+
+
